6577eac4e31e42e7f4f2d4a48f9f7fc218737f92
Matt Koski <mjkekai@gmail.com>
2021-09-09 19:37:12 -0700

    fixing server.js

--- server/server.js
+++ server/server.js
@@ -13,10 +13,10 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware,
 });
-
-server.start().then(() => {
+async () => {
+  await server.start();
   server.applyMiddleware({ app });
-});
+};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -25,6 +25,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
